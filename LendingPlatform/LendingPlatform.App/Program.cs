e3937478fa9dd25529f8d3b4f37fbcced288c355
Kieran Ghataora <KieranJSG@gmail.com>
2024-07-07 23:02:57 +0100

    Initial Commit

---
+++ LendingPlatform/LendingPlatform.App/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+
+var arguments = Environment.GetCommandLineArgs();
+
+var loanValue = decimal.Parse(arguments[1]);
+var assetValue = decimal.Parse(arguments[2]);
+var creditScore = int.Parse(arguments[3]);
+
+Console.WriteLine(loanValue);
+Console.WriteLine(assetValue);
+Console.WriteLine(creditScore);
