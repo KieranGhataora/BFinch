8d3527c72c502a3e24640511a98cd1d77b2a0952
Kieran Ghataora <KieranJSG@gmail.com>
2024-07-07 23:20:44 +0100

    Implement the Above One Million logic

--- LendingPlatform/LendingPlatform.Services.Tests/ApplicationProcessorTest.cs
+++ LendingPlatform/LendingPlatform.Services.Tests/ApplicationProcessorTest.cs
@@ -25,4 +25,50 @@ public class ApplicationProcessorTest
 
         Assert.Equal(success, applicationResult);
     }
+    
+    [Theory]
+    [InlineData(0.6, true)]
+    [InlineData(0.59, true)]
+    [InlineData(0.61, false)]
+    public void ApplicationProcessor_ReturnsCorrectResult_IfLTVCriteriaNotMet_AboveOneMillion(decimal ltv, bool success)
+    {
+        const decimal loanAmount = 1000000;
+
+        var application = new Application
+        {
+            LoanValue = loanAmount,
+            AssetValue = loanAmount/ ltv,
+            CreditScore = 950
+        };
+
+        var applicationProcessor = new ApplicationProcessor();
+
+        var result = applicationProcessor.Process(application);
+
+        Assert.Equal(success, result);
+    }
+
+    [Theory]
+    [InlineData(950, true)]
+    [InlineData(951, true)]
+    [InlineData(949, false)]
+    public void ApplicationProcessor_ReturnsCorrectResult_IfCreditScoreCriteriaNotMet_AboveOneMillion(int creditScore,
+        bool success)
+    {
+        const decimal loanAmount = 1000000;
+
+        var application = new Application
+        {
+            LoanValue = loanAmount,
+            AssetValue = loanAmount / 0.5m,
+            CreditScore = creditScore
+        };
+
+        var applicationProcessor = new ApplicationProcessor();
+
+        var result = applicationProcessor.Process(application);
+
+        Assert.Equal(success, result);
+    }
+
 }
--- LendingPlatform/LendingPlatform.Services/ApplicationProcessor.cs
+++ LendingPlatform/LendingPlatform.Services/ApplicationProcessor.cs
@@ -4,10 +4,18 @@ public class ApplicationProcessor
 {
     private const decimal MaxLoan = 1500000m;
     private const decimal MinLoan = 100000m;
+    private const decimal MinLtvAboveOneMillion = 0.6m;
+    private const int MinCreditScoreAboveOneMillion = 950;
 
     public bool Process(Application application)
     {
         if (application.LoanValue is > MaxLoan or < MinLoan) return false;
+        
+        var ltv = application.LoanValue / application.AssetValue;
+     
+        if (application.LoanValue >= 1000000)
+            if (ltv > MinLtvAboveOneMillion || application.CreditScore < MinCreditScoreAboveOneMillion)
+                return false;
 
         return true;
     }
